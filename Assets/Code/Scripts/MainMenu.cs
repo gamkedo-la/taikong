622dfde2bb3e77b75e5a155998978ce9b6da2a73
Jared Rigby <jared@mtddigital.uk>
2022-08-24 21:12:27 +0800

    Created placeholder functions for the main menu and gameover page

--- Assets/Code/Scripts/MainMenu.cs
+++ Assets/Code/Scripts/MainMenu.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
-{
-    public void PlayGame() 
-    {
+public class MainMenu : MonoBehaviour {
+    public void PlayGame() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ReturnToMenu() {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ShareScore() {
+        // TODO share high-score button
+    }
+
+    public void ViewCredits() {
+        // TODO link to credits scene
     }
 }
