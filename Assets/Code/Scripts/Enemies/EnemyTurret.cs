68c3577aabcc44c8ec9a4d0a04e94ecc2c22ec2d
Jared Rigby <jared@mtddigital.uk>
2022-10-06 22:41:40 +0800

    When the player is in range of the turret's collider, it will fire a laser at them periodically - the speed of this can be adjusted in the Unity editor

--- Assets/Code/Scripts/Enemies/EnemyTurret.cs
+++ Assets/Code/Scripts/Enemies/EnemyTurret.cs
@@ -7,6 +7,10 @@ public class EnemyTurret : MonoBehaviour
     bool playerInRange = false;
     Transform weapon;
     Transform player;
+    List<Transform> turretFire;
+
+    public Transform turretLaser;
+    public float firingRate;
 
     private void OnTriggerEnter(Collider other)
     {   //Check for a match with the specific tag on any GameObject that collides with your GameObject
@@ -27,6 +31,7 @@ public class EnemyTurret : MonoBehaviour
 
     private void Start() {
         weapon = this.gameObject.transform.GetChild(0).GetChild(0).transform;
+        InvokeRepeating("CreateLaser", 1.0f, firingRate);
     }
 
     private void Update() {
@@ -38,6 +43,17 @@ public class EnemyTurret : MonoBehaviour
         } else {
             Quaternion weaponLook = Quaternion.LookRotation(weapon.transform.position);
             weapon.rotation = Quaternion.Slerp(weapon.rotation, weaponLook, Time.deltaTime);
+        }
+    }
+
+    private void CreateLaser() 
+    {   
+        Vector3 barrelOffset = new Vector3(0, 4.2f, 0);
+        if (playerInRange) {
+            Transform laser = Instantiate(turretLaser);
+            // Get the position of the canon part of the turret model
+            laser.transform.position = weapon.transform.position + barrelOffset;
+            laser.transform.rotation = weapon.transform.rotation;
         }
     }
 }
