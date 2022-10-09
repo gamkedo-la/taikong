1d593b201ea35cb13e59a354954f9f490a188fe6
Jared Rigby <jared@mtddigital.uk>
2022-10-09 22:06:17 +0800

    Small changes during Week 5 check-in video

--- Assets/Code/Scripts/Player/PlayerBehaviour.cs
+++ Assets/Code/Scripts/Player/PlayerBehaviour.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerBehaviour : Health
+public class PlayerBehaviour : MonoBehaviour
 {
 
     private GameObject healthTextbox;
