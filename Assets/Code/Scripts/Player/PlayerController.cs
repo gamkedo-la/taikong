a9e7313128e33b375d0e6e6310ba9c3fe7f0441b
Jared Rigby <jared@mtddigital.uk>
2022-10-16 18:55:41 +0800

    KB+M controller updated - player can now fire main weapon on mouse click

--- Assets/Code/Scripts/Player/PlayerController.cs
+++ Assets/Code/Scripts/Player/PlayerController.cs
@@ -118,7 +118,7 @@ public class PlayerController : MonoBehaviour {
                 crossSpeed = mouseCrosshairSpeed;
             }
 
-            if (Input.GetKeyDown("space")) 
+            if (Input.GetKeyDown("space") || Input.GetMouseButtonDown(0)) 
             {
                 FireLasers();
             }
