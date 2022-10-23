8a6ba2c1621645ebbb85c2f83c5772bc6f754fc0
Jared Rigby <jared@mtddigital.uk>
2022-10-23 21:22:49 +0800

    Created base behaviour class for all enemies

---
+++ Assets/Code/Scripts/Enemies/EnemyBaseClass.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBaseClass : MonoBehaviour, IEnemyBehaviour
+{
+    [SerializeField] AudioSource fireWeaponSound;
+    [SerializeField] AudioSource damageSound;
+    [SerializeField] AudioSource destroyedSound;
+
+    bool playerInRange = false;
+    UnitHealth health = new UnitHealth(50, 50);
+    [SerializeField] Transform weapon;
+    Transform player;
+    public Transform turretLaser;
+    public float firingRate;
+    public int scorePoints;
+
+    private void Start() {
+        InvokeRepeating("FireWeapon", 1.0f, firingRate);
+    }
+
+    private void Update() {
+        Vector3 rest = new Vector3(0,0,0);
+
+        if (playerInRange) {
+            Quaternion weaponLook = Quaternion.LookRotation(player.transform.position - weapon.transform.position);
+            weapon.rotation = Quaternion.Slerp(weapon.rotation, weaponLook, Time.deltaTime);
+        } else {
+            Quaternion weaponLook = Quaternion.LookRotation(weapon.transform.position);
+            weapon.rotation = Quaternion.Slerp(weapon.rotation, weaponLook, Time.deltaTime);
+        }
+
+        if (health.Health <= 0) {
+            DestroySelf();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {           
+        if(other.CompareTag("PlayerDetect"))
+        {
+            LockOnPlayer(other.transform);
+            // Debug.Log("Looking at object: " + other.transform.ToString());
+        }
+
+        // CapsuleCollider is used for enemy hurtboxes
+        if(other.CompareTag("PlayerLaser")) {
+            damageSound.Play();
+            health.DamageUnit(other.GetComponent<Lasers>().laserDamage);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {   
+        if(other.CompareTag("PlayerDetect"))
+        {
+            IgnorePlayer();
+        }
+    }
+
+    public void LockOnPlayer(Transform p) 
+    {
+        playerInRange = true;
+        player = p;
+    }
+
+    public void IgnorePlayer() 
+    {
+        playerInRange = false;
+    }
+
+    public void FireWeapon() 
+    {
+        Vector3 barrelOffset = new Vector3(0, 4.2f, 0);
+
+        if (playerInRange) {
+            fireWeaponSound.Play();
+            Transform laser = Instantiate(turretLaser);
+            // Get the position of the canon part of the turret model
+            laser.transform.position = weapon.transform.position + barrelOffset;
+            laser.transform.rotation = weapon.transform.rotation;
+        }
+    }
+
+    public void DestroySelf() 
+    {
+        destroyedSound.Play();
+
+        // Add points to players score when enemy is destroyed
+        ScoreKeeper.instance.AddScore(scorePoints);
+
+        // TODO
+        // When enemy health gets to zero, trigger an explosion animation
+        // then remove the game object from the scene
+        Destroy(gameObject);
+    }
+}
