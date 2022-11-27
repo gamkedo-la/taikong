eac147a943097df4cb05ec7133524671eef0670b
Jared Rigby <talk@jaredrigby.co.uk>
2022-11-27 19:53:36 +0800

    Created logic to load the next level

---
+++ Assets/Code/Scripts/Menus/NextLevelMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevelMenu : MonoBehaviour
+{
+    private Scene activeScene; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        activeScene = SceneManager.GetActiveScene();
+    }
+
+    public void OnLevelComplete() {
+        // If the player is on level 1, 2 or 3, load the next level
+        // Else, show the gameover level for their final score
+        if (activeScene.buildIndex < 3) {
+            SceneManager.LoadScene(activeScene.buildIndex + 1);
+        } else {
+            SceneManager.LoadScene(5);
+        }
+    }
+}
