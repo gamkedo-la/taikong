e593fed8e409b7dd7a791ccae9b31d59b1ace32e
Jared Rigby <talk@jaredrigby.co.uk>
2022-12-04 14:27:45 +0800

    Created boost pickup model

---
+++ Assets/Code/Scripts/Items/ItemBase.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBase : MonoBehaviour
+{
+    private Transform pickupIcon;
+    private float rotationSpeed = 50f;
+
+    void Start() {
+        pickupIcon = transform.GetChild(0).transform;
+    }
+    
+    void Update() {
+        float currentRotation = rotationSpeed * Time.deltaTime;
+        pickupIcon.Rotate(currentRotation, -currentRotation, currentRotation);
+    }
+
+    void OnTriggerEnter(Collider other) {
+        Debug.Log("Through the loop!");
+    }
+
+    void RemoveItem() {
+        // TODO animate and remove item ring from the scene
+        Destroy(gameObject);
+    }
+}
